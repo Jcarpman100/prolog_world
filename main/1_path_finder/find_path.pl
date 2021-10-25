6c82c57717f94b5667efb1de9dcb9880af36729b
Jcarpman100 <64172253+Jcarpman100@users.noreply.github.com>
2021-10-25 13:30:39 -0500

    Comments
    Added comments to all functions, dressed up for turn in.

--- main/1_path_finder/find_path.pl
+++ main/1_path_finder/find_path.pl
@@ -1,13 +1,16 @@
 % 
 % facts can go here
 % Howdy
+% This took me a hot minute, but it works all nice now
 
 % here's a graph definition
 arc('m','p',8).
 arc('q','p',13).
 arc('q','m',5).
 arc('k','q',3).
-arc('k','p',16).
+
+% extra facts for testing.
+% arc('k','p',16).
 % arc('k','m',7).
 
 
@@ -19,21 +22,26 @@ arc('k','p',16).
 %	arc(A, B, WhatLength),
 %	write(WhatLength + cost), nl, halt(0).
 
+% Unneeded function, made to test recursion.
 countdown(0) :- write(0).
 countdown(X) :- write(X), nl, Y is X - 1, countdown(Y). 
 
+% Driver function, calls the pathfinder functions to locate all paths and the shortest path function to select the best.
 path(A,B,P,C) :- allpaths(A,B,Paths), shortestPath(Paths,[],1000,C,P).
 
+% Recursively finds the best path by checking if the head is better than the current best path.
 shortestPath([],P,C,BestC,BestP) :- BestC is C, BestP = P.
 shortestPath([Head|Paths],P,C,BestC,BestP) :- bestPath(Head,P,C,NewC,NewP), shortestPath(Paths,NewP,NewC,BestC,BestP).
 
+% Determines if this path is better, equal, or worse than the current best.
+bestPath([Hp,Hc|_],_,LastC,C,P) :- Hc < LastC, C is Hc, append([Hp],[],P). 					% If this path is better, replace the path and cost,
+bestPath([Hp,Hc|_],LastP,LastC,C,P) :- Hc =:= LastC, append([Hp],LastP,P), C is Hc. 		% If this path is equal, append it to the current best.
+bestPath([_,_|_],LastP,LastC,C,P) :- C is LastC, P = LastP. 								% If this path is worse, do nothing.
 
-bestPath([Hp,Hc|_],LastP,LastC,C,P) :- Hc < LastC, C is Hc, append([Hp],[],P).
-bestPath([Hp,Hc|_],LastP,LastC,C,P) :- Hc =:= LastC, append([Hp],LastP,P), C is Hc.
-bestPath([Hp,Hc|_],LastP,LastC,C,P) :- C is LastC, P = LastP.
-
+% uses the findall function on the pathfinder function in order to locate every possible path.
 allpaths(A,B,P) :- findall([Path,C], (pathfinder(A,B,Path,C,[A])), P).
 
+% pathfinding function, uses matching to determine a valid path.
 pathfinder(A,B,[B|P],C,P) :- arc(A,B,Cost), C is Cost.
 pathfinder(A,B,P,C,V) :- arc(A,D,Cost1), pathfinder(D,B,P,Cost2, [D|V]), C is Cost1 + Cost2.
 
@@ -50,7 +58,7 @@ pathfinder(A,B,P,C,V) :- arc(A,D,Cost1), pathfinder(D,B,P,Cost2, [D|V]), C is Co
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % uncomment the next few lines to query your path method
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-     path(StartNode, EndNode, Output, Cost),
+    path(StartNode, EndNode, Output, Cost),
     write('is: '), write(Output), write('\n'),
     write('Cost: '), write(Cost), write('\n'),
     
