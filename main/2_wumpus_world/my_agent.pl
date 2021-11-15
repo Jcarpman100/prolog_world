55a50b367d4db1a58e81c05f70f44c3ca667e3a6
Jcarpman100 <64172253+Jcarpman100@users.noreply.github.com>
2021-11-15 16:31:46 -0600

    Bloodthirst added
    The agent will make an attempt on the wumpus's life before going home.

--- main/2_wumpus_world/my_agent.pl
+++ main/2_wumpus_world/my_agent.pl
@@ -12,7 +12,7 @@
 % This is what should be fleshed out
 % Space fact, (X, Y, Safe, PossiblePit, PossibleWumpus)
 
-:- dynamic noStench/2, noBreeze/2, stench/2, breeze/2, visited/2, wall/2, safety/2, haveArrow/0, murder/0.
+:- dynamic noStench/2, noBreeze/2, stench/2, breeze/2, visited/2, wall/2, safety/2, haveArrow/0, murder/0, home/0.
 
 % turn facts, can turn left or right, mostly use right turns because they are the right way to go.
 turn(north, east).
@@ -45,6 +45,7 @@ init_agent:-
 	
 
 % --------- unused pathfinding predicates ---------------------------------------------------------------------------------------
+
 % I wanted to make the agent pathfind to unvisited tiles, but I could not figure out the predicate for finding unvisited, safe spaces from anywhere on the grid.
 % I would have also made the agent pathfind to the ladder when it found the gold
 % meandering is not great, but it usually finds the gold when a safe path exists.
@@ -68,7 +69,7 @@ allpaths(A,B,P) :- findall([Path,C], (pathfinder(A,B,Path,C,[A])), P).
 pathfinder(A,B,[B|P],C,P) :- arc(A,B,Cost), C is Cost.
 pathfinder(A,B,P,C,V) :- arc(A,D,Cost1), pathfinder(D,B,P,Cost2, [D|V]), C is Cost1 + Cost2.
 
-
+% --------- helper predicates -----------------------------------------------------------------------------------------------------
 
 % Tiled faced
 % Pretty self explanatory, gives the next tile using the direction fact and given tile.
@@ -76,7 +77,6 @@ next_tile(X, Y, Dx, Dy) :- direction(east), Dx is X + 1, Dy is Y.
 next_tile(X, Y, Dx, Dy) :- direction(north), Dx is X, Dy is Y + 1.
 next_tile(X, Y, Dx, Dy) :- direction(west), Dx is X - 1, Dy is Y.
 next_tile(X, Y, Dx, Dy) :- direction(south), Dx is X, Dy is Y - 1.
-
 
 
 % Confirmed Wumpus?
@@ -88,19 +88,25 @@ conWumpus(X,Y) :- X2 is X + 1, Y1 is Y - 1, stench(X2, Y), stench(X, Y1), not(vi
 conWumpus(X,Y) :- X2 is X + 1, Y2 is Y + 1, stench(X2, Y), stench(X, Y2), not(visited(X, Y)).
 conWumpus(X,Y) :- Y1 is X + 1, Y2 is Y + 1, stench(X, Y1), stench(X, Y2), not(visited(X, Y)).
 
+
 % Possible Wumpus?
 % if none of the neighboring spaces lack a stench, we must assume a wumpus.
-wumpus(X, Y) :- not(visited(X, Y)), X1 is X - 1, X2 is X + 1, Y1 is Y - 1, Y2 is Y + 1, not(noStench(X1, Y)), not(noStench(X2, Y)), not(noStench(X, Y1)), not(noStench(X, Y2)).
+wumpus(X, Y) :- not(visited(X, Y)), X1 is X - 1, X2 is X + 1, Y1 is Y - 1, Y2 is Y + 1,
+not(noStench(X1, Y)), not(noStench(X2, Y)), not(noStench(X, Y1)), not(noStench(X, Y2)).
+
 
 % Possible Pit?
 % if none of the neighboring spaces lack a breeze, we must assume a wumpus.
-pit(X, Y) :- not(visited(X, Y)), X1 is X - 1, X2 is X + 1, Y1 is Y - 1, Y2 is Y + 1, not(noBreeze(X1, Y)), not(noBreeze(X2, Y)), not(noBreeze(X, Y1)), not(noBreeze(X, Y2)).
+pit(X, Y) :- not(visited(X, Y)), X1 is X - 1, X2 is X + 1, Y1 is Y - 1, Y2 is Y + 1,
+not(noBreeze(X1, Y)), not(noBreeze(X2, Y)), not(noBreeze(X, Y1)), not(noBreeze(X, Y2)).
+
 
 % Is Safe?
 % Is the tile safe? changes depending on the state of the wumpus and arrow.
 isSafe(X,Y) :- not(pit(X,Y)), not(wumpus(X,Y)).
 isSafe(X,Y) :- not(pit(X,Y)), murder.
 isSafe(X,Y) :- not(pit(X,Y)), safety(X,Y).
+
 
 % Are there any safe places still unvisited?
 % Attempted but was unable to make good predicate.
@@ -109,6 +115,13 @@ isSafe(X,Y) :- not(pit(X,Y)), safety(X,Y).
 
 % new_space() :- isSafe(X,Y), not(visited(X,Y)). not(wall(A,B)).
 
+
+% wumpus nearby?
+% Is one of neighboring tiles the wumpus?
+wumpusFound(X, Y) :- X1 is X - 1, conWumpus(X1,Y).
+wumpusFound(X, Y) :- X2 is X + 1, conWumpus(X2,Y).
+wumpusFound(X, Y) :- Y1 is Y - 1, conWumpus(X,Y1).
+wumpusFound(X, Y) :- Y2 is Y + 1, conWumpus(X,Y2).
 
 
 % Unvisited nearby?
@@ -121,6 +134,7 @@ unVisited(X, Y) :- Y2 is Y + 1, not(visited(X, Y2)), not(wall(X,Y2)), isSafe(X,Y
 
 % run_agent(Percept,Action):-
 
+
 % display world
 % Just print the world at every turn.
 run_agent(_, _) :- display_world, boredom(X), X1 is X + 1, replace_existing_fact(boredom(X), boredom(X1)), false().
@@ -129,27 +143,41 @@ run_agent(_, _) :- display_world, boredom(X), X1 is X + 1, replace_existing_fact
 
 % Bored
 % A new tile has not been seen for 16 moves, go home.
-run_agent([_,_,_,_,_], _) :- boredom(X), X >= 15, assert(gold(1)), write("I'm going home\n"), false().
+run_agent([_,_,_,_,_], _) :- not(home(1)), boredom(X), X >= 15, assert(home(1)), write("I'm going home\n"), false().
+
+
+% All objectives complete.
+% If the agent has the gold and has attempted to kill the wumpus, it may go home.
+run_agent([_,_,_,_,_], _) :- not(home), gold(1), murder, assert(home), false().
+
+run_agent([_,_,_,_,_], _) :- not(home), gold(1), not(haveArrow), assert(home), false().
+
 
 % Scream!
 % Assert murder.
 run_agent([_,_,_,_,yes], _) :- assert(murder), false().
 
+
 % Bump
 % retract the perceived movement, turn, and assert the location as a wall.
-run_agent([_,_,_,yes,_], turnright):- location(X,Y), retract(visited(X,Y)), assert(wall(X,Y)), next_tile(X, Y, Dx, Dy), DDx is (2 * X) - Dx, DDy is (2 * Y) - Dy, replace_existing_fact(location(X,Y), location(DDx, DDy)),
+run_agent([_,_,_,yes,_], turnright):- location(X,Y), retract(visited(X,Y)), assert(wall(X,Y)), 
+next_tile(X, Y, Dx, Dy), DDx is (2 * X) - Dx, DDy is (2 * Y) - Dy, replace_existing_fact(location(X,Y), location(DDx, DDy)),
 write("bump\n"), direction(A), turn(A,B), write("turn!"), replace_existing_fact(direction(A), direction(B)).
+
 
 % Wumpus nearby
 % If there is a wumpus nearby, assert a stench
 % If there is not a wumpus nearby, assert a noStench
 run_agent([yes,_,_,no,_], _):- location(X,Y), assert(stench(X, Y)), write("I smell a Wumpus.\n"),false().
+
 run_agent([no,_,_,no,_], _):- location(X,Y), assert(noStench(X, Y)), write("Smells fine.\n"), false().
+
 
 % Pit nearby
 % If there is a pit nearby, assert nothing.
 % If there is not a pit nearby, assert a noBreeze.
 run_agent([_,yes,_,no,_], _):- write("I feel a breeze.\n"), false().
+
 run_agent([_,no,_,no,_], _):- location(X,Y), assert(noBreeze(X, Y)), write("The air is still.\n"), false().
 
 
@@ -157,50 +185,77 @@ run_agent([_,no,_,no,_], _):- location(X,Y), assert(noBreeze(X, Y)), write("The 
 
 % Wumpus ahead
 % Take the shot if you have an arrow and mark the location as safe.
-run_agent([yes,_,_,_,_], shoot):- haveArrow, location(X,Y), next_tile(X, Y, Dx, Dy), conWumpus(Dx,Dy), write("The wumpus is in my sight!.\n"), assert(safety(Dx, Dy)), retract(haveArrow).
+run_agent([yes,_,_,_,_], shoot):- haveArrow, location(X,Y), next_tile(X, Y, Dx, Dy), conWumpus(Dx,Dy),
+write("The wumpus is in my sight!.\n"), assert(safety(Dx, Dy)), retract(haveArrow).
+
+
+% Murder
+% If there is a known wumpus location is nearby, turn to commit murder.
+run_agent(_,turnright):- haveArrow, location(X,Y), wumpusFound(X,Y), replace_existing_fact(boredom(G),
+boredom(0)), direction(A), turn(A,B), replace_existing_fact(direction(A), direction(B)), write("I found the wumpus!").
+
 
 % On Gold
 % Grab that money.
 run_agent([_,_,yes,_,_], grab):- gold(0), write("grab\n"), assertz(gold(1)).
 
+
 % On ladder with breeze?
 % Climb out, no risks will be taken
 run_agent([_,yes,_,_,_], climb):- location(0,0), write("Not worth falling into a pit!\n") .
 
+
 % On ladder with stench?
 % Use your arrow to carve a safe path.
-run_agent([yes,_,_,_,_], shoot):- location(0,0), haveArrow, next_tile(0, 0, Dx, Dy), assert(safety(Dx, Dy)), retract(haveArrow), write("I'm in a corner with the beast!.\n").
+run_agent([yes,_,_,_,_], shoot):- location(0,0), haveArrow, next_tile(0, 0, Dx, Dy), assert(safety(Dx, Dy)),
+retract(haveArrow), write("I'm in a corner with the beast!.\n").
+
 
 % On ladder with gold!
 % Leave, we have won!
-run_agent([_,_,_,_,_], climb):- location(0,0), gold(1).
+run_agent([_,_,_,_,_], climb):- location(0,0), home.
 
 
 % --------- common movement predicates ---------------------------------------------------------------------------------------
 
 % Pit danger!
 % If there could be a pit ahead, turn right.
-run_agent([_,_,_,_,_], turnright):- location(X,Y), next_tile(X, Y, Dx, Dy), pit(Dx, Dy), write("A pit is ahead.\n"), direction(A), turn(A,B), replace_existing_fact(direction(A), direction(B)).
+run_agent([_,_,_,_,_], turnright):- location(X,Y), next_tile(X, Y, Dx, Dy), pit(Dx, Dy), write("A pit is ahead.\n"),
+direction(A), turn(A,B), replace_existing_fact(direction(A), direction(B)).
+
 
 % Wumpus danger!
 % If there could be a live wumpus ahead, turn right.
-run_agent([_,_,_,_,_], turnright):- location(X,Y), next_tile(X, Y, Dx, Dy), not(murder), not(safety(Dx,Dy)), wumpus(Dx, Dy), write("A wumpus is ahead.\n"), direction(A), turn(A,B), replace_existing_fact(direction(A), direction(B)).
+run_agent([_,_,_,_,_], turnright):- location(X,Y), next_tile(X, Y, Dx, Dy), not(murder), not(safety(Dx,Dy)),
+wumpus(Dx, Dy), write("A wumpus is ahead.\n"), direction(A), turn(A,B), replace_existing_fact(direction(A), direction(B)).
+
 
 % Avoid known walls
 % If there is a wall ahead, turn right
-run_agent(_,turnright):- location(X,Y), next_tile(X, Y, Dx, Dy), wall(Dx, Dy), direction(A), turn(A,B), replace_existing_fact(direction(A), direction(B)), write("Wall ahead.").
+run_agent(_,turnright):- location(X,Y), next_tile(X, Y, Dx, Dy), wall(Dx, Dy), direction(A), turn(A,B),
+replace_existing_fact(direction(A), direction(B)), write("Wall ahead.").
+
 
 % Meander home
 % If you have the gold and the next tile has been visited, move forward.
-run_agent(_,goforward):- location(X,Y), next_tile(X, Y, Dx, Dy), gold(1), visited(Dx,Dy), replace_existing_fact(location(X,Y), location(Dx, Dy)), write("forward!").
+run_agent(_,goforward):- location(X,Y), next_tile(X, Y, Dx, Dy), home, visited(Dx,Dy),
+replace_existing_fact(location(X,Y), location(Dx, Dy)), write("forward!").
+
 
 % Meander to the gold
 % If you do not have the gold and you haven't visited a nearby tile, turn
-run_agent(_,turnright):- location(X,Y), next_tile(X, Y, Dx, Dy), gold(0), unVisited(X, Y), replace_existing_fact(boredom(G), boredom(0)), visited(Dx, Dy),  direction(A), turn(A,B), replace_existing_fact(direction(A), direction(B)), write("Unvisited tile nearby!").
+run_agent(_,turnleft):- location(X,Y), next_tile(X, Y, Dx, Dy), home, not(visited(Dx, Dy)),  direction(A),
+turn(B,A), replace_existing_fact(direction(A), direction(B)), write("Unvisited tile ahead, turn!").
+
+run_agent(_,turnright):- location(X,Y), next_tile(X, Y, Dx, Dy), not(home(1)), unVisited(X, Y),
+replace_existing_fact(boredom(G), boredom(0)), visited(Dx, Dy),  direction(A), turn(A,B),
+replace_existing_fact(direction(A), direction(B)), write("Unvisited tile nearby!").
+
 
 % Default Moving
 % If nothing else has occurred, walk forward.
-run_agent(_,goforward):- location(X,Y), next_tile(X, Y, Dx, Dy),assert(visited(Dx, Dy)), replace_existing_fact(location(X,Y), location(Dx, Dy)), write("forward!").
+run_agent(_,goforward):- location(X,Y), next_tile(X, Y, Dx, Dy),assert(visited(Dx, Dy)),
+replace_existing_fact(location(X,Y), location(Dx, Dy)), write("forward!").
 
 
 
